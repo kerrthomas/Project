18bd37b630762d3604fa6c993e5c2cddbabc2c0e
Andrew Kerr <andrewkerr3956@gmail.com>
2022-07-23 12:39:47 -0400

    testing

--- api/hello.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const connection = require('./../../db');
-
-export default function handler(req, res) {
-  connection.default.query("SELECT * FROM user", (err, results) => {
-    res.json(results);
-  });
-
-}
