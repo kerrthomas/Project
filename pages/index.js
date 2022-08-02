ddb163a487a54b90658ba9ddbb900a5f891a7870
kerrthomas <78629459+kerrthomas@users.noreply.github.com>
2022-08-02 16:38:26 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,21 @@
-import styles from '../styles/Home.module.css';
-import { useEffect, useState } from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
 function App() {
-
-  useEffect(() => {
-    const test = async() => {
-      const result = await fetch('/api/hello');
-      result = await result.json();
-      console.log("data retrieved: " + result.pass);
-    }
-
-    console.log("used Effect")
-    test();
-  }, []);
-
-  const [category, setCategory] = useState([]);
-
-  function selectCat() {
-
-  };
-
   return (
     <div className={styles.container}>
       <header className={styles.headerAlign}>
         <h1>Questions and Answers</h1>
         <span>Login</span>
       </header>
-      <main>
-        <div className={styles.catContainer}>
-          <button className={styles.catTab} onClick={selectCat}>Category 1</button>
-          <button className={styles.catTab} onClick={selectCat}>Category 2</button>
-          <button className={styles.catTab} onClick={selectCat}>Category 3</button>
-          <button className={styles.catTab} onClick={selectCat}>Category 4</button>
-          <button className={styles.catTab} onClick={selectCat}>Category 5</button>
-        </div>
-        <div className={styles.questContainer} style={{color: 'red', fontSize: '10vh', boxShadow: '0 0 150px #000000'}}>KISS YOUR HOMIES GOOD NIGHT</div>
-      </main>
+      <div className={styles.catContainer}>
+        <button className={styles.catTab}>Category 1</button>
+        <button className={styles.catTab}>Category 2</button>
+        <button className={styles.catTab}>Category 3</button>
+        <button className={styles.catTab}>Category 4</button>
+        <button className={styles.catTab}>Category 5</button>
+      </div>
     </div>
   );
 }
